3b5de8545ffc2a56515f56e2f8b1e3bee71bd71c
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-05-22 18:35:36 -0700

    add CinderBlock skeleton

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cinder-NoamLemma
+`Cinder-NoamLemma` is a client implementation for the [Noam protocol](http://noam.io/).
---
+++ src/Cinder-NoamLemma.h
@@ -0,0 +1,9 @@
+//
+//  Cinder-NoamLemma.h
+//  SmokeSignals
+//
+//  Created by Jean-Pierre Mouilleseaux on 22 May 2014.
+//  Copyright 2014 Chorded Constructions. All rights reserved.
+//
+
+#pragma once
