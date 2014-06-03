04c5794ec971fef5e5183342012fddcc596166d7
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-06-03 12:02:28 -0700

    change header blob project name

--- src/Cinder-NoamProtocol.h
+++ src/Cinder-NoamProtocol.h
@@ -1,6 +1,6 @@
 //
 //  Cinder-NoamProtocol.h
-//  SmokeSignals
+//  Cinder-NoamProtocol
 //
 //  Created by Jean-Pierre Mouilleseaux on 22 May 2014.
 //  Copyright 2014 Chorded Constructions. All rights reserved.
--- src/Lemma.cpp
+++ src/Lemma.cpp
@@ -1,6 +1,6 @@
 //
 //  Lemma.cpp
-//  SmokeSignals
+//  Cinder-NoamProtocol
 //
 //  Created by Jean-Pierre Mouilleseaux on 22 May 2014.
 //  Copyright 2014 Chorded Constructions. All rights reserved.
